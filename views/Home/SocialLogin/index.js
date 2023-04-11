c249054eecb09e882619499109346e800ac2c36f
RafoPetrosyan <petrosyanrafo0@gmail.com>
2023-04-11 11:19:01 +0400

    changes

--- views/Home/SocialLogin/index.js
+++ views/Home/SocialLogin/index.js
@@ -2,7 +2,6 @@ import React, {useState} from 'react';
 import {LoginSocialGoogle, LoginSocialFacebook} from 'reactjs-social-login';
 import {FacebookLoginButton, GoogleLoginButton} from 'react-social-login-buttons';
 import {FACEBOOK_APP_ID, GOOGLE_CLIENT_ID} from "../../../constants";
-import axios from "axios";
 
 const SocialLogin = () => {
     const [profile, setProfile] = useState(null);
@@ -10,17 +9,8 @@ const SocialLogin = () => {
     console.log(profile, 777)
     console.log(provider, 88888)
 
-    const onResolve = async (data) => {
+    const onResolve = (data) => {
         setProfile(data);
-       await axios.post('http://localhost:4000/users/social_login', {
-            token: data?.access_token
-        })
-            .then(function (response) {
-                console.log(response);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
     }
 
     return (
@@ -43,6 +33,7 @@ const SocialLogin = () => {
                 appId={FACEBOOK_APP_ID}
                 onResolve={({ provider, data }) => {
                     setProvider(provider)
+
                     onResolve(data)
                 }}
                 onReject={(err) => {
